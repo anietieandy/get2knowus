ed362578ea630ba2d508e2c35a8ce90115eb931c
natashanarang <nnarang777@gmail.com>
2017-11-05 16:27:11 -0500

    basic query view

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Get2KnowUS' });
 });
 
+router.post('/submit_query', function(req, res, next) {
+	console.log(req.body.query_field);
+  	res.render('query_results', { title: 'Get2KnowUS', query: req.body.query_field, results: ["dummy result 1", "dummy result 2"] });
+});
+
 
 module.exports = router;
