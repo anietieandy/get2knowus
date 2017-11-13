cb65c6a77a445709eefa2ea793eb092fe301656f
natashanarang <nnarang777@gmail.com>
2017-11-13 16:21:22 -0500

    basic querying

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Imports the Google Cloud client library.
+const Storage = require('@google-cloud/storage');
+// Imports the Google Cloud client library
+const BigQuery = require('@google-cloud/bigquery');
+// The project ID to use, e.g. "your-project-id"
+const projectId = "green-entity-183800";
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -14,8 +22,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,66 +38,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-
-// Imports the Google Cloud client library.
-const Storage = require('@google-cloud/storage');
-
-// Instantiates a client. Explicitly use service account credentials by
-// specifying the private key file. All clients in google-cloud-node have this
-// helper, see https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/latest/guides/authentication
-const storage = Storage({
-  keyFilename: 'My First Project-b75eb6aaa18f.json'
-});
-
-// Makes an authenticated API request.
-storage
-  .getBuckets()
-  .then((results) => {
-    const buckets = results[0];
-
-    console.log('Buckets:');
-    buckets.forEach((bucket) => {
-      console.log(bucket.name);
-    });
-  })
-  .catch((err) => {
-    console.error('ERROR:', err);
-  });
-
-// Imports the Google Cloud client library
-const BigQuery = require('@google-cloud/bigquery');
-
-// The project ID to use, e.g. "your-project-id"
-const projectId = "bigquerytest-185903";
-
-// The SQL query to run
-const sqlQuery = `SELECT author, name, subreddit, body
-FROM `fh-bigquery:reddit_comments.2015_05`
-WHERE author != "[deleted]" AND author
-IN (SELECT author FROM `fh-bigquery:reddit_comments.2015_05` WHERE LENGTH(body) < 255 AND LENGTH(body) > 30 AND body LIKE '%m a dad%') LIMIT 500;`;
-
-// Instantiates a client
-const bigquery = BigQuery({
-  projectId: projectId
-});
-
-// Query options list: https://cloud.google.com/bigquery/docs/reference/v2/jobs/query
-const options = {
-  query: sqlQuery,
-  useLegacySql: false // Use standard SQL syntax for queries.
-};
-
-// Runs the query
-bigquery
-  .query(options)
-  .then((results) => {
-    const rows = results[0];
-    printResult(rows);
-  })
-  .catch((err) => {
-    console.error('ERROR:', err);
-  });
 
 // error handlers
 
@@ -115,6 +62,97 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+
+
+var credentials = process.env;
+
+if (!process.env.PRODUCTION) {
+	credentials = require('./credentials.json');
+}
+
+//add credentials for Google Cloud
+var config = {
+	projectId: projectId,
+	credentials: credentials
+};
+
+// The SQL query to run
+const sqlQuery = 
+"SELECT author, name, subreddit, body FROM `fh-bigquery.reddit_comments.2015_05` WHERE author != '[deleted]' AND author IN (SELECT author FROM `fh-bigquery.reddit_comments.2015_05` WHERE LENGTH(body) < 255 AND LENGTH(body) > 30 AND body LIKE '%m a dad%') LIMIT 500;";
+
+
+// Instantiates a client
+const bigquery = BigQuery({
+  projectId: projectId,
+  credentials: credentials
+});
+
+// Query options list: https://cloud.google.com/bigquery/docs/reference/v2/jobs/query
+const options = {
+  query: sqlQuery,
+  useLegacySql: false // Use standard SQL syntax for queries.
+};
+
+function printResult (rows) {
+  console.log('Query Results:');
+  for (var i = 0; i < rows.length; i++) {
+  	row = rows[i];
+  	console.log("\n---------------------");
+  	console.log("Author: " + row.author);
+  	console.log("Name: " + row.name);
+  	console.log("Subreddit: " + row.subreddit);
+  	console.log("Body: " + row.body);  	
+  }
+}
+
+// Runs the query
+bigquery
+  .query(options)
+  .then((results) => {
+    const rows = results[0];
+    printResult(rows);
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
+
+
+
+// Instantiates a client. Explicitly use service account credentials by
+// specifying the private key file. All clients in google-cloud-node have this
+// helper, see https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/latest/guides/authentication
+/*
+const storage = Storage({
+  keyFilename: 'My First Project-b75eb6aaa18f.json'
+});
+*/
+
+// Makes an authenticated API request.
+/*
+storage
+  .getBuckets()
+  .then((results) => {
+    const buckets = results[0];
+
+    console.log('Buckets:');
+    buckets.forEach((bucket) => {
+      console.log(bucket.name);
+    });
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });	*/
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = app;
