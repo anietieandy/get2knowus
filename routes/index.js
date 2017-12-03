37dcf79c457a004b84a672781b7c215d67933047
natashanarang <nnarang777@gmail.com>
2017-12-02 23:12:45 -0500

    index changes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var sqlQuery = "SELECT author, name, subreddit, body FROM `fh-bigquery.reddit_comments.2015_05` WHERE LENGTH(body) < 255 AND LENGTH(body) > 30 AND body LIKE '%";
+var sqlQuery = "SELECT author, name, subreddit, body FROM `fh-bigquery.reddit_comments.2015_05` WHERE author != '[deleted]' AND LENGTH(body) < 255 AND LENGTH(body) > 30 AND body LIKE '%";
+
+var usernameQuery = "SELECT body FROM `fh-bigquery.reddit_comments.2015_05` WHERE author IN (";
 
 // Imports the Google Cloud client library.
 const Storage = require('@google-cloud/storage');
@@ -9,6 +11,8 @@ const Storage = require('@google-cloud/storage');
 const BigQuery = require('@google-cloud/bigquery');
 // The project ID to use, e.g. "your-project-id"
 const projectId = "green-entity-183800";
+//for fs
+const fs = require('fs');
 //For exec
 var exec = require('child_process').exec;
 
@@ -29,50 +33,139 @@ const bigquery = BigQuery({
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Get2KnowUS',
-  						err: '' });
+  						args: {}
+  						});
+});
+
+router.get('/download', function(req,res) {
+	res.sendFile("all_posts.txt", { root: '.' });
 });
 
 router.post('/submit_query', function(req, res, next) {
-	runClassifier()
-	curr_query = req.body.query_field
-	if (curr_query.includes("'")) {
-		res.render('index', { title: 'Get2KnowUS', 
-							  err: "Please provide a phrase that does not contain '."})
+	var curr_query = req.body.query_field;
+	if (!req.body.hidden) {
+		var new_options = ["I'm a father"]
+		if (new_options.length > 0) {
+			res.render('index', { title: 'Get2KnowUS',
+								  args: {help: "We found some other queries you may find helpful - check to see if you'd like to add them!",
+	  									 other_queries: new_options,
+	  									 query_to_enter: curr_query}
+	  							});
+		}
+	} else {
+		var all_queries = [curr_query];
+		var new_query = curr_query.replace(/'/gi, "\\'");
+		var query_to_enter = sqlQuery + new_query + "%'";
+		for (query in req.body) {
+			if (query != 'hidden' && query != 'query_field') {
+				all_queries.push(query);
+				query = query.replace(/'/gi, "\\'");
+				query_to_enter += " OR body LIKE '%" + query + "%'";
+			}
+		}
+		query_to_enter += " LIMIT 500;";
+
+		var options = {
+			query: query_to_enter,
+			useLegacySql: false
+		};
+		console.log("if this prints once, a callback is being called twice!!!!");
+		runQuery(options, (rows) => {
+			var str = ""
+			for (var i = 0; i < rows.length; i++) {
+				str += rows[i].body.replace(/(\r\n|\n|\r)/gm,"") + "\n";
+			}
+			fs.writeFile('query_results.txt', str, function(err) {
+				console.log("about to query");
+				exec('python2 classifier/sdg1.py query_results.txt', (err, stdout, stderr) => {
+					if (err) {
+						console.log(err);
+						return;
+					}
+					var all_results = stdout.split("##########")[1];
+					all_results = all_results.replace(/(\r\n|\n|\r)/gm,"");
+					all_results = all_results.substring(1,all_results.length);
+					all_results = all_results.split(" ");
+					var new_username_query = usernameQuery;
+					for (var i = 0; i < all_results.length; i++) {
+						if (all_results[i] == "1") {
+							new_username_query += "'" + rows[i].author + "', ";
+						}
+					}
+					new_username_query = new_username_query.substring(0, new_username_query.length - 2);
+					new_username_query += ") AND body != '[deleted]' LIMIT 500;";
+					console.log(new_username_query);
+					var username_options = {
+						query: new_username_query,
+						useLegacySql: false
+					};
+					runQuery(username_options, (rows) => { //FIX THIS
+						var new_str = ""
+						for (var i = 0; i < rows.length; i++) {
+							new_str += rows[i].body.replace(/(\r\n|\n|\r)/gm,"") + "\n";
+						}						
+						fs.writeFile('all_posts.txt', new_str, function(err) {
+							if (err) {
+								console.log(err);
+								return;
+							}
+							res.render('query_results', { title: 'Get2KnowUS', all_queries: all_queries, results: rows });
+						});
+					});
+				});
+			});
+		});		
 	}
-	query_to_enter = sqlQuery + curr_query;
-	query_to_enter += "%' LIMIT 500;";
-	console.log(query_to_enter);
-	var options = {
-		query: query_to_enter,
-		useLegacySql: false
-	};
-	bigquery
-	.query(options)
-	.then((results) => {
-		rows = results[0];
-		console.log(rows);
-		res.render('query_results', { title: 'Get2KnowUS', query: curr_query, results: rows });
-		})
-	.catch((err) => {
-		console.error('ERROR:', err);
-	});
+});
+
+router.post('/classify_query', function(req, res, next) {
+	var curr_query = req.body.query_field;
+	if (!req.body.hidden) {
+		new_options = ["I'm a father"]
+		if (new_options.length > 0) {
+			res.render('index', { title: 'Get2KnowUS',
+								  args: {help: "We found some other queries you may find helpful - check to see if you'd like to add them!",
+	  									 other_queries: new_options,
+	  									 query_to_enter: curr_query}
+	  							});
+		}
+	} else {
+		var all_queries = [];
+		all_queries.push(curr_query);		
+		var new_query = curr_query.replace(/'/gi, "\\'");
+		var query_to_enter = sqlQuery + new_query + "%'";
+		for (query in req.body) {
+			if (query != 'hidden' && query != 'query_field') {
+				all_queries.push(query);
+				query = query.replace(/'/gi, "\\'");
+				query_to_enter += " OR body LIKE '%" + query + "%'";
+			}
+		}
+		query_to_enter += " LIMIT 500;";
+
+		var options = {
+			query: query_to_enter,
+			useLegacySql: false
+		};
+		runQuery(options, (rows) => {
+			res.render('classify_queries', { title: 'Get2KnowUS', queries: all_queries, results: rows });		
+		});			
+	}	
 });
 
 
-function runQuery(options) {
+function runQuery(options, callback) {
 	bigquery
 	  .query(options)
-	  .then((results) => {
-	    rows = results[0];
-	    return rows;
-	  })
+	  .then((results) => results[0])
+	  .then(callback)
 	  .catch((err) => {
 	    console.error('ERROR:', err);
 	  });	
 }
 
 function runClassifier() {
-	exec('python classifier/nb1.py', function(err, stdout, stderr) {
+	exec('python classifier/nb1.py', (err, stdout, stderr) => {
 	  if (err) {
 	    return;
 	  }
